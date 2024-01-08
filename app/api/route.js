9d88df3777ee0210769e74fc5974ea7a90c96f71
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2024-01-08 17:20:49 -0500

    V7

--- app/api/route.js
+++ app/api/route.js
@@ -1,3 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: "dputhhzyb",
+  api_key: "645242162575664",
+  api_secret: "hXQFGVBItNRiiJ7DPD_8B4Avmew",
+});
+
 export async function POST() {}
